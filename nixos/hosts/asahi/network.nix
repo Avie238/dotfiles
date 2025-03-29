c440c3783fa4918b18383fed6b5702b4bdc056e4
Avie238 <ania.dymowska238@gmail.com>
2025-03-29 12:23:27 +0100

    hotspot

--- nixos/hosts/asahi/network.nix
+++ nixos/hosts/asahi/network.nix
@@ -24,6 +24,27 @@
           psk = "ghf56^agtg_7";
         };
       };
+      iphoneHotspot = {
+        connection = {
+          id = "Ania’s iPhone";
+          permissions = "";
+          type = "wifi";
+        };
+        wifi = {
+          mode = "infrastructure";
+          ssid = "65;110;105;97;226;128;153;115;32;105;80;104;111;110;101;";
+        };
+        wifi-security = {
+          auth-alg = "open";
+          key-mgmt = "wpa-psk";
+          psk = "Anka1234";
+        };
+        ipv4.method = "auto";
+        ipv6 = {
+          addr-gen-mode = "default";
+          method = "auto";
+        };
+      };
     };
   };
 
