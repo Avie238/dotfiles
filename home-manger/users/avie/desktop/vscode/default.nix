19e4143084f48e8d2dac361e1b7042c6dd1fd545
Avie238 <ania.dymowska238@gmail.com>
2025-04-12 17:24:45 +0200

    rearange extensions

--- home-manger/users/avie/desktop/vscode/default.nix
+++ home-manger/users/avie/desktop/vscode/default.nix
@@ -17,72 +17,81 @@
 
       extensions =
         (with pkgs.vscode-marketplace; [
+          #JS/TS
           anbuselvanrocky.bootstrap5-vscode
           bradlc.vscode-tailwindcss
-          chflick.firecode
           christian-kohler.npm-intellisense
-          donjayamanne.python-extension-pack
+          chflick.firecode
           dsznajder.es7-react-js-snippets
           equimper.react-native-react-redux
-          esbenp.prettier-vscode
-          formulahendry.code-runner
-          gitlab.gitlab-workflow
-          graphql.vscode-graphql-syntax
-          hars.cppsnippets
-          ivhernandez.vscode-plist
-          james-yu.latex-workshop
-          jeff-hykin.better-cpp-syntax
+          ms-vscode.vscode-typescript-next
+          wix.glean
+          xabikos.javascriptsnippets
+          #Python
+          donjayamanne.python-extension-pack
           kevinrose.vsc-python-indent
-          kleber-swf.unity-code-snippets
-          mechatroner.rainbow-csv
-          misodee.vscode-nbt
-          ml.nc-gcode
-          ms-azuretools.vscode-docker
+          ms-python.black-formatter
           ms-python.debugpy
           ms-python.isort
           ms-python.python
           ms-python.vscode-pylance
+          ms-toolsai.jupyter
           ms-toolsai.jupyter-keymap
           ms-toolsai.jupyter-renderers
           ms-toolsai.vscode-jupyter-cell-tags
           ms-toolsai.vscode-jupyter-slideshow
+          percy.vscode-numpy-viewer
+          percy.vscode-pydata-viewer
+          #C++
+          hars.cppsnippets
+          jeff-hykin.better-cpp-syntax
           ms-vscode.cpptools
           ms-vscode.cpptools-extension-pack
           ms-vscode.cpptools-themes
-          ms-vscode.vscode-typescript-next
-          mtxr.sqltools
-          mtxr.sqltools-driver-mysql
-          percy.vscode-numpy-viewer
-          percy.vscode-pydata-viewer
-          pkief.material-icon-theme
-          quicktype.quicktype
-          redhat.java
-          sergey-tihon.openxml-explorer
-          shahilkumar.docxreader
-          shd101wyy.markdown-preview-enhanced
-          softwaredotcom.swdc-vscode
-          tamasfe.even-better-toml
-          tobiah.unity-tools
-          tomoki1207.pdf
-          visualstudioexptteam.intellicode-api-usage-examples
-          visualstudioexptteam.vscodeintellicode
+          #Java
           vscjava.vscode-gradle
           vscjava.vscode-java-debug
           vscjava.vscode-java-dependency
           vscjava.vscode-java-pack
           vscjava.vscode-java-test
-          wayou.vscode-todo-highlight
-          wix.glean
-          xabikos.javascriptsnippets
-          yclepticstudios.unity-snippets
-          zainchen.json
+          redhat.java
+          #Nix
           jnoortheen.nix-ide
-
+          #Git
+          gitlab.gitlab-workflow
+          eamodio.gitlens
+          #Unity
+          tobiah.unity-tools
+          yclepticstudios.unity-snippets
+          kleber-swf.unity-code-snippets
+          #Sql
+          mtxr.sqltools
+          mtxr.sqltools-driver-mysql
+          #General
+          esbenp.prettier-vscode
+          formulahendry.code-runner
+          graphql.vscode-graphql-syntax
+          ivhernandez.vscode-plist
+          james-yu.latex-workshop
+          mechatroner.rainbow-csv
+          misodee.vscode-nbt
+          ml.nc-gcode
+          ms-azuretools.vscode-docker
+          pkief.material-icon-theme
+          quicktype.quicktype
+          sergey-tihon.openxml-explorer
+          shahilkumar.docxreader
+          shd101wyy.markdown-preview-enhanced
+          softwaredotcom.swdc-vscode
+          tamasfe.even-better-toml
+          tomoki1207.pdf
+          visualstudioexptteam.intellicode-api-usage-examples
+          visualstudioexptteam.vscodeintellicode
+          wayou.vscode-todo-highlight
+          zainchen.json
         ])
         ++ (with pkgs.vscode-extensions; [
-          ms-toolsai.jupyter
-          ms-python.black-formatter
-          eamodio.gitlens
+          #C#
           ms-dotnettools.csdevkit
           ms-dotnettools.csharp
           ms-dotnettools.vscode-dotnet-runtime
