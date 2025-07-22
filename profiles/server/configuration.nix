f98f656d787cf539efaf4a860a9fa67a8e917b46
Avie238 <ania.dymowska238@gmail.com>
2025-07-22 14:21:34 +0200

    a

--- profiles/server/configuration.nix
+++ profiles/server/configuration.nix
@@ -2,6 +2,7 @@
   imports = [
     (userSettings.dotfilesDir + "/system/minimal/ssh.nix")
     (userSettings.dotfilesDir + "/system/minimal/users.nix")
+    (userSettings.dotfilesDir + "/system/minimal/sops.nix")
     (userSettings.dotfilesDir + "/system/server")
   ];
 }
