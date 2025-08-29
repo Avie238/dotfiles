a2c904bd5af29af550492142dd0629797c2692e2
Avie238 <ania.dymowska238@gmail.com>
2025-08-29 23:20:11 +0200

    a

--- profiles/server/configuration.nix
+++ profiles/server/configuration.nix
@@ -4,4 +4,9 @@
     (userSettings.dotfilesDir + "/system/server")
     (userSettings.dotfilesDir + "/system/desktop/stylix.nix")
   ];
+  programs.git = {
+    enable = true;
+    userName = "Avie238";
+    userEmail = "ania.dymowska238@gmail.com";
+  };
 }
