e8d307ddfbd2d7bf0bfe9e4e415c66d600635955
Avie238 <ania.dymowska238@gmail.com>
2025-08-29 23:22:08 +0200

    a

--- profiles/server/configuration.nix
+++ profiles/server/configuration.nix
@@ -6,7 +6,7 @@
   ];
   programs.git = {
     enable = true;
-    userName = "Avie238";
-    userEmail = "ania.dymowska238@gmail.com";
+    # userName = "Avie238";
+    # userEmail = "ania.dymowska238@gmail.com";
   };
 }
