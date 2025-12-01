58afef9a01558583c6ea68cdd5811ff4bda98efe
Avie238 <ania.dymowska238@gmail.com>
2025-12-01 17:05:10 +0100

    a

--- system/server/samba.nix
+++ system/server/samba.nix
@@ -3,16 +3,13 @@
     enable = true;
     enableNmbd = false;
     enableWinbindd = false;
-    extraConfig = ''
-      guest account = myuser
-      map to guest = Bad User
+    settings.global = {
+      "guest account" = "myuser";
+      "map to guest" = "Bad User";
 
-      load printers = no
-      printcap name = /dev/null
-
-      log file = /var/log/samba/client.%I
-      log level = 2
-    '';
+      "load printers" = "no";
+      "printcap name" = "/dev/null";
+    };
 
     shares = {
       nas = {
