777bbc792457232d24775aa2758c30185d8be0de
Avie238 <ania.dymowska238@gmail.com>
2026-05-30 21:39:17 +0200

    a

--- profiles/server/home.nix
+++ profiles/server/home.nix
@@ -1,6 +1,12 @@
-{userSettings, ...}: {
+{
+  userSettings,
+  lib,
+  ...
+}: {
   imports = [
     (userSettings.dotfilesDir + "/user/shell")
     (userSettings.dotfilesDir + "/user/app/editor/nvim")
   ];
+
+  home.stateVersion = lib.mkForce "26.05";
 }
