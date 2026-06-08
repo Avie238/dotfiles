127804d77c8631a32140969237d48ad8ba398f8c
Avie238 <ania.dymowska238@gmail.com>
2026-06-08 14:46:50 +0200

    a

--- flake.nix
+++ flake.nix
@@ -101,12 +101,16 @@
     nixosSystemFor = userSettings:
       nixpkgs.lib.nixosSystem {
         pkgs = userSettings.pkgs;
-        modules =
-          [
-            (userSettings.systemModule)
-            inputs.niri.nixosModules.niri
-          ]
-          ++ [inputs.home-manager.nixosModules.home-manager self.nixosModules.my-user];
+        modules = [
+          (userSettings.systemModule)
+          inputs.niri.nixosModules.niri
+          self.nixosModules.my-user
+          (
+            if userSettings.stable
+            then inputs.home-manager-stable.nixosModules.home-manager
+            else inputs.home-manager.nixosModules.home-manager
+          )
+        ];
         specialArgs = {
           inherit inputs userSettings self;
         };
@@ -268,7 +272,6 @@
     steam-nixpkgs.url = "github:dramforever/nixpkgs/muvm-steam-less-hacks";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-26.05";
-    nixpkgs-24.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-avie.url = "github:Avie238/nixpkgs";
 
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
@@ -280,6 +283,11 @@
 
     home-manager = {
       url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager-stable = {
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
